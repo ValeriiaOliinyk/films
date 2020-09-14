8e73961ee0b5be09ae308b45b6656da76bf4a28f
Valeriia Oliinyk <olijnykvaleria@gmail.com>
2020-09-14 12:52:13 +0300

    Updates

---
+++ precache-manifest.b6580b657589481f2d63eb886a0453da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54793d7d10acdefaa54c610327b6aa3e",
+    "url": "/films/index.html"
+  },
+  {
+    "revision": "890f942c4ce940b9090f",
+    "url": "/films/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "2a709ee14d87c9f03483",
+    "url": "/films/static/css/main.8987054c.chunk.css"
+  },
+  {
+    "revision": "890f942c4ce940b9090f",
+    "url": "/films/static/js/2.3e81d7b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/films/static/js/2.3e81d7b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a709ee14d87c9f03483",
+    "url": "/films/static/js/main.427a267d.chunk.js"
+  },
+  {
+    "revision": "2241fa07bacf1f64e7b3",
+    "url": "/films/static/js/runtime-main.05a2359d.js"
+  },
+  {
+    "revision": "7ed36fce2317353a6d57c1eb08b90785",
+    "url": "/films/static/media/information.7ed36fce.svg"
+  },
+  {
+    "revision": "5157c40c4cd9a575d5e61bc866b918a7",
+    "url": "/films/static/media/plus.5157c40c.svg"
+  },
+  {
+    "revision": "c76f17cdfc78fc451aceaac4d81b4eb1",
+    "url": "/films/static/media/sort.c76f17cd.svg"
+  },
+  {
+    "revision": "6cdae8ca4301f0dc5b10381bc616080d",
+    "url": "/films/static/media/trash.6cdae8ca.svg"
+  }
+]);
